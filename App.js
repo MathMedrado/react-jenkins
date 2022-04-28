57ce990601873d336fd05a7c2395d083171179e9
Matheus Medrado <matheus-13-@hotmail.com>
2022-04-28 13:49:12 -0300

    adicionando app

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Expo is crazy</Text>
+      <Text>I am the bone of my sword</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#AAA222',
     alignItems: 'center',
     justifyContent: 'center',
   },
